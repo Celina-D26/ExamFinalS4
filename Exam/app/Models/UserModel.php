bc4f3d3cab68197f264845474beda42223ee2c13
Lovasoa Celina <celinarakotobe@gmail.com>
2026-07-20 08:42:50 +0300

    login number

--- Exam/app/Models/UserModel.php
+++ Exam/app/Models/UserModel.php
@@ -6,16 +6,36 @@ use CodeIgniter\Model;
 
 class UserModel extends Model
 {
-    public function authenticate($username, $password)
+    protected $table = 'users';
+    protected $primaryKey = 'id';
+    protected $allowedFields = ['phone_number', 'name', 'email', 'last_login', 'login_count'];
+    protected $useTimestamps = true;
+    protected $createdField = 'created_at';
+    protected $updatedField = 'updated_at';
+
+    public function findOrCreateByPhone($phoneNumber)
     {
-        $user = $this->where('username', $username)->first();
-
-        if ($user && password_verify($password, $user['password'])) {
-            return $user;
+        $user = $this->where('phone_number', $phoneNumber)->first();
+        
+        if (!$user) {
+            // Créer un nouvel utilisateur automatiquement
+            $data = [
+                'phone_number' => $phoneNumber,
+                'name' => 'Utilisateur ' . substr($phoneNumber, -4),
+                'login_count' => 1,
+                'last_login' => date('Y-m-d H:i:s')
+            ];
+            
+            $this->insert($data);
+            return $this->where('phone_number', $phoneNumber)->first();
         }
-
-        return false;
+        
+        // Mettre à jour les informations de connexion
+        $this->update($user['id'], [
+            'login_count' => $user['login_count'] + 1,
+            'last_login' => date('Y-m-d H:i:s')
+        ]);
+        
+        return $user;
     }
-
-
 }
